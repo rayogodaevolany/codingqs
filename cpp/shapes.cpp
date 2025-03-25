2df84ee198ff3d8564a404213f2779feaaaa1595
rayogoda <ray.ogoda@evolany.com>
2025-03-25 10:08:42 +0900

    Finish shapes
    It's not a hundred percent but I think this is fine.

---
+++ cpp/shapes.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+int s1(){
+    int num = 4;
+
+    for(int i = 0; i < num; i++){
+        for(int m = 0; m < i; m++){
+            cout << " ";
+        }
+        for(int t = 0; t < 8 - 2*i; t++){
+            cout << "#";
+            }
+        cout << "\n";
+    }
+
+    return 0;
+}
+int s2(){
+    int num = 4;
+
+    for(int i = 0; i < num; i++){
+        for(int m = 0; m < 4 - i; m++){
+            cout << " ";
+        }
+        for(int t = 0; t < 2*i; t++){
+            cout << "#";
+            }
+        cout << "\n";
+    }
+    
+    s1();
+    return 0;
+}
+
+int s3top() {
+    int num = 5;
+
+    for(int i = 0; i < num; i++){
+        for(int m = 0; m < i; m++){
+            cout << " ";
+        }
+        for(int t = 0; t < i; t++){
+            cout << "#";
+            }
+        for(int m = 0; m < (4 - i) * 4; m++){
+            cout << " ";
+        }
+        for(int t = 0; t < i; t++){
+            cout << "#";
+            }
+        cout << "\n";
+    }
+    return 0;
+}
+
+int s3bottom() {
+    int num = 5;
+
+    for(int i = 0; i < num; i++){
+        for(int m = 0; m < 4 - i; m++){
+            cout << " ";
+        }
+        for(int t = 0; t < 4 - i; t++){
+            cout << "#";
+            }
+        for(int m = 0; m < i * 4; m++){
+            cout << " ";
+        }
+        for(int t = 0; t < 4 - i; t++){
+            cout << "#";
+            }
+        cout << "\n";
+    }
+    return 0;
+}
+int main () {
+    s3top();
+    s3bottom();
+
+
+
+}
