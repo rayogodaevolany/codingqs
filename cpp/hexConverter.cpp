cc0c9305a851ff3bfe3ecd0234e8bee16bd1e7e4
rayogoda <ray.ogoda@evolany.com>
2025-04-04 21:41:40 +0900

    Add partial hexConverter

--- cpp/hexConverter.cpp
+++ cpp/hexConverter.cpp
@@ -5,18 +5,16 @@ using namespace std;
 
 // this routine converts a stringified number into an int number
 int convertToInt(string str) {
-    // declare number, current variable
+    // declare number, current variable, length of string
     int num = 0, cur;
-    // find length of str
     int len = str.length();
-    // for length of str from i to end of str convert each to int
+    // for the length of the string
     for (int i=0; i<len;i++){
-        // find int from string at position i
+        // get the int in the string at index i
         cur = str[i] - '0';
         // multiply the original number by ten and add int
         num = (num * 10) + cur;
     }
-    // return num
     return num;
 }
 
@@ -24,23 +22,21 @@ int convertToInt(string str) {
 char singleHex(int digit) {
     // list of characters in base 16
     char list[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-    // return character at the right index based on input num
+    // return hex converted character
     return list[digit];
 }
 
 // this routine converts a stringified decimal number into a stringified hex
 string convertToHex(string num){
-    // declare results variable
     string res;
-    // declare char vector hexNum
     vector<char> hexNum;
-    // declare copy of number, quotient
     int copy, quotient;
-    // copy num in int form
+    // convert inputted string to int form
     copy = convertToInt(num);
-    // while copy is not zero
+
+    // while the original number is still valid
     while (copy != 0) {
-    //     get the quotient from dividing number by 16
+    //     get the quotient 
         quotient = copy/16;
     //     convert quotient into hex character
         quotient = singleHex(quotient);
@@ -61,12 +57,11 @@ string convertToHex(string num){
 }
 
 int main() {
-    // declare string str
-    // declare string hexstr
     string str, hexstr;
-    // ask user for input
-    // store input in str
-    // call input with converToHex
-    // print hexstr
+    cout << "Enter a decimal number" << endl;
+    cin >> str;
+
+    hexstr = convertToHex(str);
+    cout << hexstr;
     return 0;
 }
