25cfc6d9eef2278aa402b7c187d7b8739e53251a
rayogoda <ray.ogoda@evolany.com>
2025-04-21 14:19:19 +0900

    20250421 lunch

---
+++ cpp/atcoder/coins.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+// find out how many ways there are to make a number X with coins A,B, and C
+int main(){
+    // can you use modulo?
+    // 100 % 100 is 0 then its good?
+    // 100 / 100 is 1 so only one?
+    // coins is recursively found no?
+    // no that was just if its possible or not
+    //
+    //
+}
