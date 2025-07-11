1771c69fb58ebd5be4f00bc4848212f91c07f12f
rayogoda <ray.ogoda@evolany.com>
2025-07-11 18:25:03 +0900

    Add update

--- cpp/atcoder/effortfulLearning/cppversion/20250707_abc_C_145_averagelength.cc
+++ cpp/atcoder/effortfulLearning/cppversion/20250707_abc_C_145_averagelength.cc
@@ -3,33 +3,40 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+#include <iomanip>
 using namespace std;
 
 double distance(pair<int, int> r, pair<int, int> l){
     return sqrt(pow((r.first - l.first), 2) + (pow(r.second - l.second, 2)));
 }
-
+// list of coordinates -> average
+// get the average distance between all of these coordinates
 void solve(){
     int n; cin >> n;
     vector<pair<int, int>> arr(n);
     vector<int> order(n);
+    // create an ordered array from 0 to n -1;
     for (int i = 0; i < n; i++) order[i] = i;
+
+    // in the arr of pairs put in the coordinates
     for (int i = 0; i < n; i++){
         int x, y; cin >> x >> y;
         arr[i].first = x;
         arr[i].second = y;
     }
+
     double sum = 0;
     double count = 0;
+    // for every permutation of array order
     do {
-        for (int i = 0; i < n; i++){
-            sum += distance(arr[order[i]], arr[order[i+1]]);
+        for (int i = 0; i < (n -1); i++){
+            sum += distance(arr[order[i+1]], arr[order[i]]);
         }
         count++;
     } while (next_permutation(order.begin(), order.end()));
 
     double average = sum / count;
-    cout << average << endl;
+    cout << setprecision(10) << average << endl;
 }
 
 int main(){
--- cpp/atcoder/effortfulLearning/cppversion/20250709_abc_C_385_illuminatebuildings.cc
+++ cpp/atcoder/effortfulLearning/cppversion/20250709_abc_C_385_illuminatebuildings.cc
@@ -12,18 +12,42 @@ using namespace std;
 // output the number of elements that are the same
 void solve(){
     int n; cin >> n;
+    // val : {index, ...}
     unordered_map<int, vector<int>> hash;
     vector<int> arr(n);
     for (int i = 0; i < n; i++){
         cin >> arr[i];
-        hash[arr[i]].push_back(i);
+        // take the interval
+        hash[arr[i]].push_back(i - *hash[arr[i]].end());
     }
 
-    for (int i = 0; i < (int)hash.size(); i++){
-        int interval = hash[arr[i]] - 0;
-        while(true){
+    int res = 0;
+    // for every hash key
+    for (int j = 0; j < (int)hash.size(); j++){
+        // from every element as the starting
+        for (int i = 0; i < (int)hash[j].size(); i++){
+            int interval = 0;
+            // for every element in the vector
+            for (int m = 0; m < ((int)hash[j].size() - i); m++){
+                interval += hash[j][m];
+                int count = 0;
+                int nextinterval = 0;
+                int increment = 1;
+                // until out of bounds
+                while(hash[j][i+increment] && interval > nextinterval){
+                    nextinterval+=hash[j][i+increment];
+                    if (interval == nextinterval){
+                        nextinterval = 0;
+                        count++;
+                    }
+                    increment++;
+                }
+                res = max(count, res);
+            }
+            interval = 0;
         }
     }
+    cout << res << endl;
 }
 
 int main(){
